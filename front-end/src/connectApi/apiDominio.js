5ea99e3174c54093a4eac5e908f7be973e23c32f
yurign000 <yurign000@gmail.com>
2023-07-09 18:33:31 -0300

    feat: change api host

--- front-end/src/connectApi/apiDominio.js
+++ front-end/src/connectApi/apiDominio.js
@@ -1,1 +1,5 @@
-export const dominio = 'http://localhost:3000/'
+var local = 'http://localhost:3000/';
+var localtunnel = 'https://electraverse-api.loca.lt/'; //lt --port 3000 --subdomain electraverse-api
+var render = 'https://electraverse-api.onrender.com/' //render.com
+
+export const dominio = render;
